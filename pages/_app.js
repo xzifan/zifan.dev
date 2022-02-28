045fef17ef2595adf8ca6da4a599978b7be31c6a
xzifan <xiao@zifan.dev>
2022-02-28 20:53:07 +0800

    fix: mdx

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import Script from 'next/script'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -15,6 +16,10 @@ const isSocket = process.env.SOCKET
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Script
+        type="text/javascript"
+        src="https://s4.cnzz.com/z_stat.php?id=1280913104&web_id=1280913104"
+      ></Script>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
